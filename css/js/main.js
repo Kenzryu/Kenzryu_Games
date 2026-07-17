322e5f0700f678fd0426c4fa1287a6ebf2d12cdc
Kenzryu Games <rcops12@gmail.com>
2026-07-18 00:26:20 +0700

    Implement lightbox functionality for gallery images

--- css/js/main.js
+++ css/js/main.js
@@ -1,1 +1,38 @@
 console.log("Kenzryu Games Website Loaded");
+/* ============================
+   LIGHTBOX
+============================ */
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeLightbox = document.querySelector(".close-lightbox");
+
+document.querySelectorAll(".gallery img").forEach(img => {
+
+    img.addEventListener("click", function(){
+
+        lightbox.style.display = "flex";
+
+        lightboxImg.src = this.src;
+
+        lightboxImg.alt = this.alt;
+
+    });
+
+});
+
+closeLightbox.addEventListener("click", function(){
+
+    lightbox.style.display = "none";
+
+});
+
+lightbox.addEventListener("click", function(e){
+
+    if(e.target === lightbox){
+
+        lightbox.style.display = "none";
+
+    }
+
+});
