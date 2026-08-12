dc96dc7da75ff4b5b261b7a9fb35e251b265fe49
Kenzryu Games <rcops12@gmail.com>
2026-08-13 00:19:00 +0700

    Display latest games in descending order

--- js/latest-games.js
+++ js/latest-games.js
@@ -1,1 +1,25 @@
+const games = window.games;
 
+const latestGamesContainer = document.getElementById("latestGames");
+
+const latestGames = [...games]
+    .sort((a, b) => new Date(b.date) - new Date(a.date))
+    .slice(0, 6);
+
+
+latestGames.forEach(game => {
+
+    const card = document.createElement("div");
+
+    card.className = "game-card";
+
+    card.innerHTML = `
+        <a href="${game.url}">
+            <img src="${game.image}" alt="${game.name}">
+            <h3>${game.name}</h3>
+        </a>
+    `;
+
+    latestGamesContainer.appendChild(card);
+
+});
